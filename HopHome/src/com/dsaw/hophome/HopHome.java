ef864ca9f8d5ab9281b17296f82ffb0b13e28369
Andrew <Andrew@Warden-Laptop>
2014-03-09 17:56:25 -0700

    Saved prefs.

--- HopHome/src/com/dsaw/hophome/HopHome.java
+++ HopHome/src/com/dsaw/hophome/HopHome.java
@@ -3,13 +3,23 @@ package com.dsaw.hophome;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-
+import com.badlogic.gdx.Preferences;
 import com.badlogic.gdx.graphics.Texture;
 
 public class HopHome extends Game implements ApplicationListener {
 
+	Preferences pref;
+	
 	@Override
 	public void create() {
+		pref = Gdx.app.getPreferences("HopHome_Pref");
+		int highscore = pref.getInteger("highscore", -1);
+		if (highscore == -1) {
+			Assets.highscore = 0;
+		}
+		else {
+		   Assets.highscore = highscore;	
+		}
 		Gdx.app.debug("HopHome", "Entered the create");
 		Assets.load();
 		Texture.setEnforcePotImages(false);
@@ -19,6 +29,8 @@ public class HopHome extends Game implements ApplicationListener {
 
 	@Override
 	public void dispose() {
+		pref.putInteger("highscore", Assets.highscore);
+		pref.flush();
 		super.dispose();
 	}
 
